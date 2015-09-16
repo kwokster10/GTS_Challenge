57bf7b03c066f0e84deb55ee97e38840ace40553
kwokster10 <kwokster10@hotmail.com>
2015-09-16 15:33:32 -0400

    correcting ip4 format

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,8 +1,14 @@
+require "ipaddr"
+
 class ConnectionsController < ApplicationController
 
 	# showing all connections
 	def index
 		@engine = Engine.find(params[:engine_id])
 		@connections = Connection.where({engineName: @engine.engineName})
+		@ips = []
+		@connections.each do |connection| 
+			@ips << IPAddr.new(connection.ip, Socket::AF_INET)
+		end
 	end 
 end
--- app/controllers/engines_controller.rb
+++ app/controllers/engines_controller.rb
@@ -3,8 +3,7 @@ class EnginesController < ApplicationController
 	# showing all engines
 	def index
 			@engines = Engine.all
-			print @engines
-			# binding.pry
+			binding.pry
 	end 
 
 end
